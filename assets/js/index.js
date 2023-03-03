7b8d2f0208f3b0bf59181a7740c279bebb280e8f
annaeliava <annaeliavad@gmail.com>
2023-03-04 02:43:35 +0300

    added input range

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,42 @@
 // menu button
 let btn = document.getElementById("btnMenu");
-let closeBtn = document.getElementsByClassName("menu__closeBtn");
+let closeBtn = document.querySelector(".menu__closeBtn");
 let menu = document.querySelector(".menu__subnav-list");
 
 function showMenu() {
     if(menu.style.display === 'none'){
         menu.style.display = 'flex';
+        closeBtn.style.display = 'block';
     } else {
         menu.style.display = 'none';
+        closeBtn.style.display = 'none';
     }
 }
 
 function closeMenu() {
     menu.style.display = 'none';
+    closeBtn.style.display='none';
 }
 
 btn.addEventListener("click", showMenu);
-closeBtn[0].addEventListener("click", closeMenu);
+closeBtn.addEventListener("click", closeMenu);
 
+// input[type="range"]
+
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+
+function handleInputChange(e) {
+    let target = e.target
+    if (e.target.type !== 'range') {
+        target = document.getElementById('range')
+    } 
+    const min = target.min
+    const max = target.max
+    const val = target.value
+
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+    input.addEventListener('input', handleInputChange)
+})
